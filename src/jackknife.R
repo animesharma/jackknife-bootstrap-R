f696dd30c256fb49cf5fb63463bb7b7c838db284
Animesh Sharma <animesharma08+github@gmail.com>
2021-10-21 12:00:31 -0400

    Jackknife Estimator

---
+++ src/jackknife.R
@@ -0,0 +1,12 @@
+jackknife_estimator = function(data, stat_func=sd) {
+	n = length(data)
+    t = stat_func(data)
+    jack = sapply(1 : n, function(i) stat_func(data[-i]))
+    t_bar = mean(jack)
+    est_bias = (n - 1) * (t_bar - t) 
+    est_se = sqrt((n - 1) * mean((jack - t_bar)^2))
+    return(c(est_bias, est_se))
+}
+
+temp = 1:10
+print(jackknife_estimator(temp, mean))
