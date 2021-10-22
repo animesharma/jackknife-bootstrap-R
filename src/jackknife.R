29905b797730275bc7a9f8e3a9c56bb3ad7f221d
Animesh Sharma <animesharma08+github@gmail.com>
2021-10-22 19:02:34 -0400

    Formatting Improvements

--- src/jackknife.R
+++ src/jackknife.R
@@ -1,12 +1,12 @@
-jackknife_estimator = function(data, stat_func=sd) {
+jackknife_estimator = function(data, stat_func = sd) {
 	n = length(data)
-    t = stat_func(data)
-    jack = sapply(1 : n, function(i) stat_func(data[-i]))
-    t_bar = mean(jack)
-    est_sd = sd(jack)
-    est_bias = (n - 1) * (t_bar - t) 
-    est_se = sqrt((n - 1) * mean((jack - t_bar)^2))
-    return(c(est_sd, est_bias, est_se))
+	t = stat_func(data)
+	jack = sapply(1 : n, function(i) stat_func(data[-i]))
+	t_bar = mean(jack)
+	est_sd = sd(jack)
+	est_bias = (n - 1) * (t_bar - t) 
+	est_se = sqrt((n - 1) * mean((jack - t_bar)^2))
+	return(c(est_sd, est_bias, est_se))
 }
 
 temp = 1:10
