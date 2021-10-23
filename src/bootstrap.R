14c8f3f31a3d494104f50612f52a57fb1315fc5b
Animesh Sharma <animesharma08+github@gmail.com>
2021-10-23 16:48:54 -0400

    Updated return value

--- src/bootstrap.R
+++ src/bootstrap.R
@@ -13,27 +13,40 @@ bootstrap_estimator = function(data, stat_func = sd, n_boot = 10000) {
 	data_mean = mean(data)
 	data_sd = sd(data)
 	stat = stat_func(data)
-	boot = sapply(1:n_boot, bootstrap_operations, data, n, data_mean, stat_func)
+	# Calculate bootstrap values and store them in a Bootstrap Dataframe
+	boot = sapply(1 : n_boot, bootstrap_operations, data, n, data_mean, stat_func)
 	boot_df = as.data.frame(t(boot))
-	colnames(boot_df) = c("BootVal", "BootBiasVal", "SampleSD", "SampleStat")
+	colnames(boot_df) = c("BootVector", "BootBiasVector", "SampleSD", "SampleStat")
 	print(head(boot_df))
-	est_bias = mean(boot_df$BootBiasVal)
+	# Calculate the estimated standard deviation, bias and statistic
 	est_sd = mean(boot_df$SampleSD)
+	est_bias = mean(boot_df$BootBiasVector)
 	est_stat = mean(boot_df$SampleStat)
+	# Calculate bootstrap variance and standard error
 	bootstrap_var = mean(sapply(1 : n_boot, function(i) (boot_df$SampleStat[i] - est_stat)^2))
 	bootstrap_se = sqrt(bootstrap_var)
 	#bookversion
-	lq=quantile(bootstrap_var,.025)
-	uq=quantile(bootstrap_var,.975)
-	boot_piv_conf=(2*samle_mean-uq,2*sample_mean-lq)
-	boot_norm_conf=(sample_mean-2*bootstrap_se,sample_mean+2*bootstrap_se)
-	boot_per_conf=(lq,uq)
+	lq=quantile(boot_df$BootVector,.025)
+	uq=quantile(boot_df$BootVector,.975)
+	boot_piv_conf=c(2*data_mean-uq,2*data_mean-lq)
+	boot_norm_conf=c(data_mean-2*bootstrap_se,data_mean+2*bootstrap_se)
+	boot_per_conf=c(lq,uq)
 	#professorversion(no percentile)
-        #here professor's piv is sample_mean-(sample_sd/sqrt(n))*uq, but according to the book,I think it shoud be 2*sample
-	boot_piv_conf=(2*sample_mean-(sample_sd/sqrt(n))*uq,2*sample_mean-(sample_sd/sqrt(n))*lq)
-	boot_norm_conf=(sample_mean-(sample_sd/sqrt(n))*qnorm(0.975),sample_mean+(sample_sd/sqrt(n))*qnorm(0.975))
+    #here professor's piv is sample_mean-(sample_sd/sqrt(n))*uq, but according to the book,I think it shoud be 2*sample
+	boot_piv_conf_2=c(2*data_mean-(data_sd/sqrt(n))*uq,2*data_mean-(data_sd/sqrt(n))*lq)
+	boot_norm_conf_2=c(data_mean-(data_sd/sqrt(n))*qnorm(0.975),data_mean+(data_sd/sqrt(n))*qnorm(0.975))
 	#my guess
-	boot_per_conf=((sample_sd/sqrt(n))*uq,(sample_sd/sqrt(n))*lq)
+	boot_per_conf_2=c((data_sd/sqrt(n))*uq,(data_sd/sqrt(n))*lq)
+	return(c(
+		bootstrap_se, 
+		est_bias, 
+		boot_piv_conf,
+		boot_norm_conf,
+		boot_per_conf,
+		boot_piv_conf_2,
+		boot_norm_conf_2,
+		boot_per_conf_2
+	))
 }
 
-print(bootstrap_estimator(1:10))
+print(bootstrap_estimator(1:10, mean))
