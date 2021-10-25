6b2a1b36126278197863553468522aa3bb72c674
Vipul Gharde <vipul.gharde@gmail.com>
2021-10-25 00:57:56 -0400

    Refactor jackknife.R

--- src/jackknife.R
+++ src/jackknife.R
@@ -1,24 +1,29 @@
-jackknife_estimator = function(data, stat_func = sd) {
-	n = length(data)
-	t = stat_func(data)
-	# Calculate the jackknife vector by resampling data with one value removed
-	jack = sapply(1 : n, function(i) stat_func(data[-i]))
-	# Calculate t_bar as the mean of the jackknife vector
-	t_bar = mean(jack)
-	# print(t_bar)
-	# Calculate the estimated standard deviation, bias and standard error
-	est_sd = sd(jack)
-	est_bias = (n - 1) * (t_bar - t) 
-	est_se = sqrt((n - 1) * mean((jack - t_bar)^2))
-	# Calculate the 95th percentile confidence interval for the Jackknife estimator
-	ci_lb = t_bar - (1.96 * est_se)
-	ci_ub = t_bar + (1.96 * est_se)
-	# Return a vector of calculated values
-	return(c(
-		est_sd, 
-		est_bias, 
-		est_se, 
-		ci_lb, 
-		ci_ub
-	))
+jackknife_estimator <- function(data, stat_func = sd) {
+  n <- length(data)
+  t <- stat_func(data)
+  
+  # Calculate Jackknife Vector by Resampling Data with One Value Removed
+  jack <- sapply(1:n, function(i)
+    stat_func(data[-i]))
+  
+  # Calculate t_bar as Mean of Jackknife Vector
+  t_bar <- mean(jack)
+  
+  # Estimate Bias, Standard Deviation and Standard Error
+  est_bias <- (n - 1) * (t_bar - t)
+  est_sd <- sd(jack)
+  est_se <- sqrt((n - 1) * mean((jack - t_bar) ^ 2))
+  
+  # Calculate 95% Confidence Interval for the Jackknife Estimator
+  ci_lb <- t_bar - (1.96 * est_se)
+  ci_ub <- t_bar + (1.96 * est_se)
+  
+  # Return Results
+  return (list(
+    est_bias = est_bias,
+    est_sd = est_sd,
+    est_se = est_se,
+    ci_lb = ci_lb,
+    ci_ub = ci_ub
+  ))
 }
