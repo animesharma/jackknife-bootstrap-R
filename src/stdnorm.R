c45c02dc34713b9695f0e34ea382dda73adb77ce
Vipul Gharde <vipul.gharde@gmail.com>
2021-10-25 00:27:57 -0400

    Refactor stdnorm.R

--- src/stdnorm.R
+++ src/stdnorm.R
@@ -1,13 +1,13 @@
 std_norm_dist <- function(data) {
   # Calculate Sample Mean and Sample Standard Deviation from the Data
-  sample_mean = mean(data)
-  sample_sd = sd(data)
+  sample_mean <- mean(data)
+  sample_sd <- sd(data)
   
   # Calculate 95% Confidence Interval for the Standard Normal Distribution
   # 95% of the Area Under a Normal Distribution Lies Within Roughly 1.96
   # Standard Deviations of the Mean
-  ci_lb = sample_mean - (1.96 * sample_sd)
-  ci_ub = sample_mean + (1.96 * sample_sd)
+  ci_lb <- sample_mean - (1.96 * sample_sd)
+  ci_ub <- sample_mean + (1.96 * sample_sd)
   
   return (c(ci_lb, ci_ub))
 }
