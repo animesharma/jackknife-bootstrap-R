8df978f4e4281afeb2a76ed63c600986cb0d7884
Vipul Gharde <vipul.gharde@gmail.com>
2021-10-24 23:42:18 -0400

    Reformat stdnorm.R

--- src/stdnorm.R
+++ src/stdnorm.R
@@ -6,8 +6,8 @@ std_norm_dist <- function(data) {
   # Calculate 95% Confidence Interval for the Standard Normal Distribution
   # 95% of the Area Under a Normal Distribution Lies Within Roughly 1.96
   # Standard Deviations of the Mean
-  lb = sample_mean - (1.96 * sample_sd)
-  ub = sample_mean + (1.96 * sample_sd)
+  ci_lb = sample_mean - (1.96 * sample_sd)
+  ci_ub = sample_mean + (1.96 * sample_sd)
   
-  return (c(lb, ub))
+  return (c(ci_lb, ci_ub))
 }
